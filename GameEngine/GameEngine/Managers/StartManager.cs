9a610cb9fd18d8b8e5c92e89a8917ebbc509da04
roberto freire <robertofreireferrazpassos@gmail.com>
2024-08-11 10:45:25 -0300

    Add fade out effect in menu

--- GameEngine/GameEngine/Managers/StartManager.cs
+++ GameEngine/GameEngine/Managers/StartManager.cs
@@ -9,7 +9,9 @@ internal class StartManager : ISceneManager
     private SpriteManager _spriteManager;
     private TextureManager _textureManager;
     private SceneManager _sceneManager; 
-    private float timer;
+    private float _timer;
+    private float _opaque = 1f;
+    private float _fadeoutTime = 5f;
 
     public StartManager(SpriteManager spriteManager, TextureManager textureManager, SceneManager sceneManager)
     {
@@ -24,21 +26,25 @@ internal class StartManager : ISceneManager
 
     public void Update(GameTime gameTime)
     {
-        timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+        var seconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _timer += seconds;
+        _opaque -= seconds/(_fadeoutTime);
 
-        if (timer >= 3f)
+        if (_timer >= _fadeoutTime)
         {
             _sceneManager.Scene = SceneEnum.MENU;
-            timer = 0f;
+            _timer = 0f;
+            _opaque = 1f;
         }
     }
 
     public void Draw(GameTime gameTime)
     {
+        var color = new Color(255, 0, 0, 255) * _opaque;
         var batch = _spriteManager.SpriteBatch;
         batch.Begin(samplerState: SamplerState.PointClamp);
-        batch.DrawString(_spriteManager.Font, "PRIEST VS DEMONS", new Vector2(300, 200), Color.Red);
-        batch.DrawString(_spriteManager.Font, "BY ROBERTO FREIRE", new Vector2(350, 250), Color.Red);
+        batch.DrawString(_spriteManager.Font, "PRIEST VS DEMONS", new Vector2(300, 200), color);
+        batch.DrawString(_spriteManager.Font, "BY ROBERTO FREIRE", new Vector2(350, 250), color);
         batch.End();
     }
 }
